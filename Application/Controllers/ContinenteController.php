955e0c25f15f833f35164f2b141fe1025e8bbb0d
wesley1263 <webdesigner.ibc@gmail.com>
2014-01-09 17:54:39 -0200

    module continente done

---
+++ Application/Controllers/ContinenteController.php
@@ -0,0 +1,88 @@
+<?php
+
+use Vendor\Core\OXE_Controller;
+use Vendor\Library\FormStyle\FormStyle;
+use Vendor\Library\Table\Table;
+use Vendor\Library\Session\Session;
+use Application\Models\Continente;
+
+class ContinenteController extends OXE_Controller{
+		
+	public function init()
+	{
+		$this->session = new Session();
+		$this->table = new Table();
+		$this->form = new FormStyle();
+		$this->model = new Continente();
+	}
+	
+	public function indexAction()
+	{
+				
+		$data['title'] = 'Titulo da Pagina';
+		$data['continentes'] = $this->model->list_all();
+		$data['form'] = $this->form;
+		$data['table'] = $this->table;
+		$data['session'] = $this->session;
+				
+		$this->view('template/head',$data);
+		$this->view('template/header');
+		$this->view('continente/index',$data);
+		$this->view('template/footer');	
+		
+	}
+	
+	public function cadContinenteAction()
+	{
+		$param = func_get_args();
+		if($param){
+			$data['continentes'] = $this->model->list_once($param[1]);
+		}
+		
+		$data['title'] = 'Titulo da Pagina';
+		$data['form'] = $this->form;
+		$data['table'] = $this->table;
+		$data['session'] = $this->session;
+				
+		$this->view('template/head',$data);
+		$this->view('template/header');
+		$this->view('continente/cadContinente',$data);
+		$this->view('template/footer');
+	}
+	
+	public function saveContinenteAction()
+	{
+		$_POST['nome_continente'] = strtoupper($_POST['nome_continente']);
+		foreach($_POST as $key => $value){
+			$_POST[$key] = strip_tags($value);
+		}
+		
+		if($_POST['id_continente'] == null){
+			if(count($this->model->findContinente($_POST['nome_continente'])) == 0){
+				if($this->model->add($_POST)){
+					$this->session->setFlashMessage('Continente cadastrado com sucesso','success');
+					$this->redirector('/continente');
+				}
+			}else{
+				$this->session->setFlashMessage('Continente já cadastrado no sistema!','error');
+				$this->redirector('/continente');
+			}
+		}else{
+			if($this->model->alter($_POST)){
+					$this->session->setFlashMessage('Dados de Continente atualizado com sucesso','success');
+					$this->redirector('/continente');
+				}
+		}
+		
+	}
+	
+	public function deleteContinenteAction()
+	{
+		$param = func_get_args();
+		if($this->model->remove($param[1])){
+			$this->session->setFlashMessage('Continente removido do sistema','success');
+			$this->redirector('/continente');
+		}
+	}
+}
+			
