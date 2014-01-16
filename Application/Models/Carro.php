9805eb86586997dabe3f41653c8a7ed6864b629f
wesley1263 <webdesigner.ibc@gmail.com>
2014-01-15 23:16:13 -0200

    ajuste em mode de carro getCambio

--- Application/Models/Carro.php
+++ Application/Models/Carro.php
@@ -48,9 +48,8 @@ class Carro extends OXE_Model {
 	
 	public function getCambio()
 	{
-		return $this->select(array('moe.*','cam.*'))
-					->from('tbl_moeda','moe')
-					->join(array('tbl_cambio'=>'cam'),'cam.id_cambio = moe.id_cambio')
+		return $this->select()
+					->from('tbl_cambio')
 					->result();
 	}
 	
